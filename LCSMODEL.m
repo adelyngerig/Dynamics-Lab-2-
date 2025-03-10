0a13819d41a78b314aec19f22f590530070c08b5
Jwzientek <jawz2406@colorado.edu>
2025-03-10 10:57:53 -0600

    Update LCSMODEL.m

--- LCSMODEL.m
+++ LCSMODEL.m
@@ -1,7 +1,7 @@
 function [v_mod] = LCSMODEL(r, d, l, theta, w)
     % Derivation of angle beta with respect to distance d and angle theta
-    beta = asind((d + r * cosd(theta)) / l);
+    beta = asind((d - r * sind(theta)) / l);
     
     % Velocity model calculation
-    v_mod = -r .* sind(theta) .* w + r .* tand(beta) .* cosd(theta);
+    v_mod = (-r .* sind(theta) .* w) - (r .* tand(beta) .* cosd(theta));
 end

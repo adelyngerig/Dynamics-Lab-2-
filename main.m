2e880c7806c4f0d05ba8566a017f49f83e3549e8
Jwzientek <jawz2406@colorado.edu>
2025-03-12 17:30:04 -0600

    Update main.m

--- main.m
+++ main.m
@@ -1,41 +1,88 @@
-%% Call LCSDATA function
-filenames = ["Test1_5pt5V"; "Test1_6pt5V"; "Test1_7pt5V"; "Test1_8pt5V"; "Test1_9pt5V"; "Test1_10pt5V"];
-
-figure();
-for i = 1: length(filenames)
-    [theta_exp, w_exp, v_exp, time] = LCSDATA(filenames(i));
-
-    subplot(2, 3, i);
-    plot(theta_exp, v_exp, 'b-', 'LineWidth', 1);
-    yline(0, 'r-', 'LineWidth', 1);
-    xlim([0 2160]);
-    ylim([-1650 2160]);
-    title("Experimental Velocity vs. Experimental Angle for " + filenames(i), 'Interpreter', 'none');
-    ylabel("Experimental Velocity (mm/s)");
-    xlabel("Experimental Angle (deg)");
-    grid on;
-end
+clc;
+clear;
+close all;
 
 %% Call LCSMODEL Function
-r = 0.75;
-d = 1.55;
-l = 2.60;
-theta = 0: 2160;
+r = 7.5;
+d = 15.5;
+l = 26.0;
+
+filenames = ["Test1_5pt5V"; "Test1_6pt5V"; "Test1_7pt5V"; "Test1_8pt5V"; "Test1_9pt5V"; "Test1_10pt5V"];
+
 
 figure();
 for i = 1: length(filenames)
     [theta_exp, w_exp, v_exp, time] = LCSDATA(filenames(i));
     
-    w = mean(w_exp);
+    w = mean(w_exp) * (pi / 180);
 
-    [v_mod] = LCSMODEL(r, d, l, theta, w);
+    [v_mod] = LCSMODEL(r, d, l, theta_exp, w);
     subplot(2, 3, i);
-    plot(theta, v_mod, 'b-', 'LineWidth', 1);
+    plot(theta_exp, v_mod, 'k-', 'LineWidth', 1);
     yline(0, 'r-', 'LineWidth', 1);
     xlim([0 2160]);
-    ylim([-1650 2160]);
+    ylim([-165 216]);
     title("Model Velocity vs. Model Angle for " + filenames(i), 'Interpreter', 'none');
-    ylabel("Model Velocity (mm/s)");
+    ylabel("Model Velocity (cm/s)");
     xlabel("Model Angle (deg)");
     grid on;
 end
+
+%% Call LCSDATA function
+
+figure();
+for i = 1: length(filenames)
+    [theta_exp, w_exp, v_exp, time] = LCSDATA(filenames(i));
+
+    w = w_exp .* (pi / 180);
+
+    [v_mod] = LCSMODEL(r, d, l, theta_exp, w);
+
+    subplot(2, 3, i);
+    plot(theta_exp, v_exp, 'g-', 'LineWidth', 1);
+    hold on;
+    plot(theta_exp, v_mod, 'k-', 'LineWidth', 1);
+    hold off;
+    yline(0, 'r-', 'LineWidth', 1);
+    xlim([0 2160]);
+    ylim([-165 216]);
+    title("Velocity vs. Angle for " + filenames(i), 'Interpreter', 'none');
+    ylabel("Velocity (cm/s)");
+    xlabel("Angle (deg)");
+    legend('Experimental Data', 'Model');
+    grid on;
+end
+
+%% Residual and Error
+
+figure();
+for i = 1: length(filenames)
+    [theta_exp, w_exp, v_exp, time] = LCSDATA(filenames(i));
+
+    w = mean(w_exp) * (pi / 180);
+
+    [v_mod] = LCSMODEL(r, d, l, theta_exp, w);
+
+    v_diff = v_exp - v_mod;
+
+    subplot(2, 3, i);
+    plot(time, v_diff, 'r-', 'LineWidth', 1);
+    yline(0, 'k-', 'LineWidth', 1);
+    ylim([-50 50]);
+    xlim([0 7]);
+    title("Residuals vs. Time for " + filenames(i), 'Interpreter', 'none');
+    ylabel("Velocity (cm/s)");
+    xlabel("Time (s)");
+    grid on;
+
+    % mean and stardard deviation
+    average(i) = mean(v_diff);
+    sigma(i) = std(v_diff);
+
+end
+
+
+
+
+
+

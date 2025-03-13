cfa78bda17369466106fe6dcba0fe267ac5f2ec6
Jwzientek <jawz2406@colorado.edu>
2025-03-13 17:42:22 -0600

    Update LCSDATA.m

--- LCSDATA.m
+++ LCSDATA.m
@@ -1,29 +1,9 @@
-%% Function for taking the data and cleaning it and plotting it so that it is values that we can use
-
-clc;
-clear;
-close all;
+function [theta_exp, w_exp, v_exp, time] = LCSDATA(filename)
 
 %% Go into the folder
 addpath("Locomotive_Data_2020/");
 
-%% Call function
-filenames = ["Test1_5pt5V"; "Test1_6pt5V"; "Test1_7pt5V"; "Test1_8pt5V"; "Test1_9pt5V"; "Test1_10pt5V"];
-
-for i = 1: size(filenames)
-    [theta_exp, w_exp, v_exp, time] = extractData(filenames(i));
-
-    figure();
-    plot(v_exp, theta_exp, 'b-', 'LineWidth', 1);
-    xlim([-2000 2500]);
-    title("Experimental Velocity vs. Experimental Angular Velocity for " + filenames(i), 'Interpreter', 'none');
-    xlabel("Experimental Velocity (mm/s)");
-    ylabel("Experimental Angular Velocity (deg/s)");
-    grid on;
-end
-
 %% Create a data extraction function
-function [theta_exp, w_exp, v_exp, time] = extractData(filename)
 
 test_data = readmatrix(filename);
 
@@ -38,7 +18,7 @@ test_data = test_data(index2:index3, :);
 
 theta_exp = test_data(:, 2);
 w_exp = test_data(:, 4);
-v_exp = test_data(:, 5);
+v_exp = test_data(:, 5) / 10;
 time = test_data(:, 1);
 
 end
